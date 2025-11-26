a7ec290e4e4627adecede2c03f14eaf730b9b650
Henrique <henriquebacampos@gmail.com>
2025-11-26 20:25:41 -0300

    montei a base do front

--- front/front.js
+++ front/front.js
@@ -1,19 +1,5 @@
-let chamou = false;
-const scr = ['landPage', 'login1', 'login2', 'posts'];
-if(!chamou){
-  for (let i = 0; i < scr.length; i++) {
-    fetch(`htmls/${scr[i]}.html`)
-      .then(res => res.text())
-      .then(html => {
-        const el = document.getElementById(scr[i]);
-        if (el) {
-          el.innerHTML = html;
-        }
-      });
-  }
-  
-
- 
-  chamou = true;
-  
-}
+fetch('/nav.html')
+.then(res => res.text())
+.then(html => {
+  document.querySelector('#nav_container').innerHTML = html;
+})
--- index.js
+++ index.js
@@ -3,8 +3,20 @@ require('dotenv').config();
 const express = require('express');
 const sqlite3 = require('sqlite3');
 const bcrypt = require('bcrypt');
+const path = require('path');  // <-- Faltava isso
 
 const app = express();
 
-const port = process.env.PORT;
- 
+// Servir a pasta front corretamente
+app.use(express.static(path.join(__dirname, "front")));
+
+const port = process.env.PORT || 3000;
+
+// Rota para a home
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "front", "landPage.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
